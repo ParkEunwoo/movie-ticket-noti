229227bcc7b988d13cf2eb313c468ec25a06198c
ParkEunwoo <dmsdn960@gmail.com>
2021-12-12 21:57:34 +0900

    용산 아이맥스 12월 4째주

--- src/main.js
+++ src/main.js
@@ -2,17 +2,18 @@ import {getMovieList, filter, template} from './movie.js';
 import {sendEmail} from './email.js';
 
 async function main() {
-  const sinchon = await getMovieList('신촌아트레온', 20211218);
+  const dateList = Array.from({length: 7}, (_, i) => i + 20211220);
+  const iparksMovieList = await Promise.all(dateList.map(v => getMovieList('용산아이파크몰', v)));
+  const imaxFilteredMovieList = iparksMovieList.map(v => filter(v, '스파이더맨-노 웨이 홈', 'IMAX LASER 2D'));
 
-  const sinchonSpider = filter(sinchon, '스파이더맨-노 웨이 홈', '2D');
-
-  if (sinchonSpider.hallList.length > 0) {
-    sendEmail(template(sinchonSpider));
+  if (imaxFilteredMovieList.some(({hallList}) => hallList.length)) {
+    const templates = imaxFilteredMovieList.map(template);
+    sendEmail(templates.join('\n\n'));
   } else {
     console.log('상영관이 없습니다.');
   }
 
-  console.log(JSON.stringify(sinchon, null, 2));
+  console.log(JSON.stringify(imaxFilteredMovieList, null, 2));
 }
 
 main();
