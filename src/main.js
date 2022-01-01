db94476168cd6c5802fa9ef170bffd9f12b32de9
EunwooPark <dmsdn960@gmail.com>
2022-01-02 01:27:00 +0900

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,16 +2,16 @@ import {getMovieList, filter, template} from './movie.js';
 import {sendEmail} from './email.js';
 import {nextDate} from './date.js';
 
-let date = 20220103;
+let date = 20220108;
 
 async function main() {
   const iparkMovieList = await getMovieList('용산아이파크몰', date);
   const imaxFilteredMovie = filter(iparkMovieList, '스파이더맨-노 웨이 홈', 'IMAX LASER 2D');
 
   if (imaxFilteredMovie.hallList.length > 0) {
-    const template = template(imaxFilteredMovie);
-    sendEmail(template);
+    sendEmail(template(imaxFilteredMovie));
     date = nextDate(date);
+    main();
   } else {
     console.log('상영관이 없습니다.');
   }
